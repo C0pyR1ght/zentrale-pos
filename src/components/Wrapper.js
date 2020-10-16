1618629a2ab99e416df4f544f6b917c2f4640811
C0pyR1ght <luca.feiser@gmail.com>
2020-10-16 17:54:14 +0200

    remove useless button on Mobile

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -75,10 +75,7 @@ class Wrapper extends Component {
                     {/* Topbar */}
                     <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
                         {/* Sidebar Toggle (Topbar) */}
-
-                        <button id="sidebarToggleTop" className="btn btn-link d-md-none rounded-circle mr-3">
-                            <i className="fa fa-bars" />
-                        </button>
+                        
                         <button type="button" id="sidebarCollapse" className="btn">
                             <i className="fas fa-align-left"></i>
                         </button>
